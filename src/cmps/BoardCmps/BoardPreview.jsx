0779a902dbf394de5a33c45e4da9df88c2e16357
yifatre <yifat.reg@gmail.com>
2024-04-03 21:21:46 +0300

    Add board preview

--- src/cmps/BoardCmps/BoardPreview.jsx
+++ src/cmps/BoardCmps/BoardPreview.jsx
@@ -1,6 +1,22 @@
+import { Link, NavLink } from 'react-router-dom'
+import star from '../../assets/img/star.svg'
+import star_outline from '../../assets/img/star_outline.svg'
 
-export function BoardPreview() {
-    return(
-        <div></div>
+export function BoardPreview({ board }) {
+
+    function onToggleStar() {
+        //TODO: add edit function here
+    }
+
+
+    return (
+        <Link to={`/board/${board._id}`}>
+            <pre>{JSON.stringify(board)}</pre>
+            <article className='board-preview'>
+                <img src={board.style.backgroundImage} alt="" />
+                <h3>{board.title}</h3>
+                <button onClick={onToggleStar}><img src={board.isStarred ? star : star_outline} alt="" /></button>
+            </article>
+        </Link>
     )
 }
