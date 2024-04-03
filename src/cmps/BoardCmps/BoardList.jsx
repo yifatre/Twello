64dca60944358ed8cf4afbc87a8fc5f036c4639b
Roi Doron <roidoron6@gmail.com>
2024-04-03 22:25:54 +0300

    Add style board list

--- src/cmps/BoardCmps/BoardList.jsx
+++ src/cmps/BoardCmps/BoardList.jsx
@@ -2,13 +2,18 @@ import { BoardPreview } from "./BoardPreview"
 
 export function BoardList({ boards }) {
     console.log(boards);
-    return (<>
-        <div>im lis</div>
-        {
-            boards.map(board => {
-               return <BoardPreview key={board._id} board={board}/>
-            })
-        }
-    </>
+    return (
+        <section className="board-list-container">
+            <ul className="board-list flex justify-center">
+                {
+                    boards.map(board => {
+                        return <li key={board._id}>
+                            <BoardPreview  board={board} />
+                        </li>
+
+                    })
+                }
+            </ul>
+        </section>
     )
 }
