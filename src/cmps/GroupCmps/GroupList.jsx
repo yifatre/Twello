3d7c808bea0558c2c361b4caeb1483a97b42d450
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-04-04 02:09:34 +0300

    Add gruop preview and styling

--- src/cmps/GroupCmps/GroupList.jsx
+++ src/cmps/GroupCmps/GroupList.jsx
@@ -2,7 +2,7 @@ import { GroupPreview } from "./GroupPreview"
 
 export function GroupList({ groups }) {
     return (
-        <ul>
+        <ul className="clean-list flex">
             {groups.map(group => {
                 return <GroupPreview key={group.id} group={group} />             
             })}
--- src/cmps/GroupCmps/GroupPreview.jsx
+++ src/cmps/GroupCmps/GroupPreview.jsx
@@ -1,6 +1,22 @@
+import { collapse_icon, ellipsis_icon, plus_icon, create_icon } from '../UtilCmps/SVGs'
 
-export function GroupPreview(group) {
+export function GroupPreview({ group }) {
+    console.log(group)
     return (
-        <li><pre>{JSON.stringify(group)}</pre></li>
+        // <li><pre>{JSON.stringify(group)}</pre></li>
+        <li className="group-preview">
+            <div className="group-header">
+                <h2 className="group-title">{group.title}</h2>
+                <button className="collapse">{collapse_icon}</button>
+                <button className="options">{ellipsis_icon}</button>
+            </div>
+            <div className="tasks-container">
+
+            </div>
+            <div className='add'>
+                <button className="add-task"><span>{plus_icon}</span>Add a card</button>
+                <button className="create-from-template">{create_icon}</button>
+            </div>
+        </li>
     )
 }
