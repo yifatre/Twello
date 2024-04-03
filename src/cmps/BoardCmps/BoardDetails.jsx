3227172cd9fc6f17b1bcbf32a979ffcd2338ab8b
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-04-03 22:55:57 +0300

    Add boardDetails

--- src/cmps/BoardCmps/BoardDetails.jsx
+++ src/cmps/BoardCmps/BoardDetails.jsx
@@ -1,6 +1,30 @@
+import { useParams } from "react-router"
+import { GroupList } from "../GroupCmps/GroupList"
+import { useEffect, useState } from "react"
+import { boardService } from "../../services/board.service.local"
+import { showErrorMsg } from "../../services/event-bus.service"
 
 export function BoardDetails() {
-    return(
-        <div></div>
+    const { boardId } = useParams()
+    const [board, setBoard] = useState(null)
+
+    useEffect(() => {
+        loadBoard()
+    }, [])
+
+    async function loadBoard() {
+        try {
+            console.log(boardId);
+            setBoard(await boardService.getById(boardId))
+        }
+        catch (err) {
+            console.error(err)
+            showErrorMsg('Cannot remove board')
+        }
+    }
+    
+    if (!board) return <div>loading</div>
+    return (
+        <GroupList groups={board.groups} />
     )
 }
