90d2b02c947f6fc4e42615b091f28f12d5253bea
Roi Doron <roidoron6@gmail.com>
2024-04-04 14:50:58 +0300

    Add home page app header

--- src/cmps/AppCmps/AppHeader.jsx
+++ src/cmps/AppCmps/AppHeader.jsx
@@ -1,17 +1,36 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export function AppHeader() {
+    const { pathname } = useLocation();
+    console.log(pathname);
 
+    if(pathname === '/')return(
+        <header className="app-header-home ">
+            <section className='justify-space-between flex' >
+        <Link to='/board'>
+        <div className='logo'>
+      
+        </div>
+        </Link>
+        <nav>
+        <Link className='a-right login-home-header' >Login</Link>
+        <Link to='/board' className='a-right free-trial'>Get Twello for free</Link>
+        </nav>
+        </section>
+        </header>
+        
+    )
     return (
         <header className="app-header flex">
-            <div className='logo'>
-                <Link to='/board'>
-                    Twello
-                </Link>
-            </div>
-            <nav>
-                <Link></Link>
-            </nav>
+        <div className='logo'>
+        <Link to='/board'>
+        Twello
+        </Link>
+        </div>
+        <nav>
+        <Link></Link>
+        </nav>
         </header>
-    )
+        
+        )
 }
