c10f7ec113a3305af1229a9161bcff688709e34f
yifatre <yifat.reg@gmail.com>
2024-04-04 17:55:27 +0300

    Fix board index layout & styling

--- src/cmps/BoardCmps/BoardList.jsx
+++ src/cmps/BoardCmps/BoardList.jsx
@@ -1,14 +1,28 @@
+import { star_outline } from "../UtilCmps/SVGs"
 import { BoardPreview } from "./BoardPreview"
 
 export function BoardList({ boards }) {
-    console.log(boards);
+    console.log(boards)
     // todo addBoard from the index
-    function addBoard(){
-        console.log('create');
+    function addBoard() {
+        console.log('create')
     }
-    return (
+    return (<>
         <section className="board-list-container">
-            <ul className="board-list flex justify-center">
+            <h3 className="flex align-center"><span>{star_outline}</span> Starred boards</h3>
+            <ul className="board-list flex justify-start clean-list">
+                {
+                    boards.filter(board => board.isStarred).map(board => {
+                        return <li key={board._id}>
+                            <BoardPreview board={board} />
+                        </li>
+                    })
+                }
+            </ul>
+        </section>
+        <section className="board-list-container">
+            <h3 className="flex align-center">Your boards</h3>
+            <ul className="board-list flex justify-start clean-list">
                 {
                     boards.map(board => {
                         return <li key={board._id}>
@@ -19,11 +33,12 @@ export function BoardList({ boards }) {
                 }
                 <li className="board-preview">
 
-                    <article className='new-board ' onClick={()=> addBoard()}>
+                    <article className='new-board ' onClick={() => addBoard()}>
                         <p>Create new board</p>
                     </article>
                 </li>
             </ul>
         </section>
+    </>
     )
 }
