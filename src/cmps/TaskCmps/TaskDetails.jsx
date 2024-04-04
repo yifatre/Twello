33a6f1966ada57e2a6cee8de1d5879b3467749f4
yifatre <yifat.reg@gmail.com>
2024-04-04 20:55:11 +0300

    Add basic elements to task details

--- src/cmps/TaskCmps/TaskDetails.jsx
+++ src/cmps/TaskCmps/TaskDetails.jsx
@@ -1,25 +1,31 @@
 import { useParams } from "react-router"
+import { DescriptionEdit } from "./DescriptionEdit"
 
-<<<<<<< Updated upstream
-export function TaskDetails() {
-    const { boardId, groupId, taskId } = useParams()
-    console.log('boardId, groupId, taskId', boardId, groupId, taskId)
-    return (<>
-        <div>Task details</div>
-        {/* Description */}
-        {/* Description edit */}
-    </>
-    )
-=======
+
 export function TaskDetails({ task }) {
+    function onAddMember(){}
+
+
     return <section className="task-details">
-        <section className="cover"></section>
-        <section className="title"></section>
-        <section className="labels"></section>
-        <section className="description"></section>
-        <section className="members"></section>
+        <section className="cover">       </section>
+        <section className="title">
+            <textarea name="title" id="title" value={task.title}></textarea>
+            {/* {task.title} */}
+        </section>
+        <section className="labels">
+            {task.memberIds.map(memberId => <div>{memberId}</div>)}
+            <button onClick={onAddMember}>+</button>
+
+        </section>
+        {/* <section className="notifications"></section> */}
+        <section className="members">
+            {task.memberIds.map(memberId => <div>{memberId}</div>)}
+            <button onClick={onAddMember}>+</button>
+        </section>
+        <section className="description">
+            <DescriptionEdit />
+        </section>
 
 
     </section>
->>>>>>> Stashed changes
 }
