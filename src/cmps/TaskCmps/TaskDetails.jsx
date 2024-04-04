9c5c5bbd2d1b11ca894b151bfffe3dee4242d1e5
yifatre <yifat.reg@gmail.com>
2024-04-04 12:14:12 +0300

    Add task details as a rout

--- src/cmps/TaskCmps/TaskDetails.jsx
+++ src/cmps/TaskCmps/TaskDetails.jsx
@@ -1,6 +1,12 @@
+import { useParams } from "react-router"
 
 export function TaskDetails() {
-    return(
-        <div></div>
+    const { boardId, groupId, taskId } = useParams()
+    console.log('boardId, groupId, taskId', boardId, groupId, taskId)
+    return (<>
+        <div>Task details</div>
+        {/* Description */}
+        {/* Description edit */}
+    </>
     )
 }
