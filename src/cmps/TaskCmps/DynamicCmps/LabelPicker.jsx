387b6d97f766e69e221e9adf963b522d0810f33f
Roi Doron <roidoron6@gmail.com>
2024-04-05 14:40:32 +0300

    Labels cmp style and layout

--- src/cmps/TaskCmps/DynamicCmps/LabelPicker.jsx
+++ src/cmps/TaskCmps/DynamicCmps/LabelPicker.jsx
@@ -1,8 +1,27 @@
+import { edit_icon } from "../../UtilCmps/SVGs";
 
 
-export function LabelPicker ({info,onUpdate}){
-        return(
-            <div></div>
-        )
-    
+export function LabelPicker({ info, onUpdate }) {
+    return (
+        <section className="picker-container">
+            <input
+                type="text"
+                name="txt"
+                placeholder="Search labels..."
+            />
+            <p>Labels</p>
+            <ul className="clean-list ul-labels">
+                <li className="flex align-center">
+                    <input className="checkbox" type="checkBox" id="checkbox1" />
+                    <label htmlFor="checkbox1"> <div style={{ backgroundColor: 'rgb(245, 205, 71)' }} className="label-picker"> Design Team</div></label>
+                    <button className="edit-label">{edit_icon}</button>
+                </li>
+            </ul>
+
+            <button className="tasks-btn labels-btn">Create a new label</button>
+            <hr className="between-btn" />
+            <button className="tasks-btn labels-btn">Enable colorblind friendly mode </button>
+        </section>
+    )
+
 }
