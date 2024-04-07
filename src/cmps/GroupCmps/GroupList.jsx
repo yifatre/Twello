800716e8092f46294291a325ce98d997eb1b59b3
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-04-07 22:55:42 +0300

    Fix ui bug when dragging

--- src/cmps/GroupCmps/GroupList.jsx
+++ src/cmps/GroupCmps/GroupList.jsx
@@ -61,7 +61,6 @@ export function GroupList({ groups, board }) {
                             <Draggable key={group.id} draggableId={group.id} index={idx}>
                                 {(provided, snapshot) =>
                                     <li key={group.id}
-                                        className={`group-preview ${group.style.themeColor || 'neutral'}`}
                                         ref={provided.innerRef}
                                         {...provided.draggableProps}
                                         {...provided.dragHandleProps}>
--- src/cmps/TaskCmps/TaskList.jsx
+++ src/cmps/TaskCmps/TaskList.jsx
@@ -1,13 +1,9 @@
 import { Droppable, Draggable } from 'react-beautiful-dnd'
-
 import { TaskPreview } from "./TaskPreview"
-import { useNavigate } from 'react-router'
 
 export function TaskList({ group, saveTask, removeTask, board }) {
-    const navigate = useNavigate()
 
     return (
-
         <Droppable droppableId={group.id} type="task">
             {(provided) =>
                 <ul className="task-list clean-list" {...provided.droppableProps} ref={provided.innerRef}>
@@ -16,31 +12,26 @@ export function TaskList({ group, saveTask, removeTask, board }) {
                         <Draggable key={task.id} draggableId={task.id} index={idx}>
 
                             {(provided, snapshot) =>
-                                    <li key={task.id}
-                                        className="task-preview"
-                                        onClick={() => navigate(`/board/${board._id}/${group.id}/${task.id}`)}
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        style={{ backgroundColor: task.style?.backgroundColor || '#ffffff' }}>
+                                <li key={task.id}
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}>
 
-                                        <TaskPreview key={task.id}
-                                            id={task.id}
-                                            task={task}
-                                            groupId={group.id}
-                                            removeTask={removeTask}
-                                            saveTask={saveTask}
-                                            board={board}
-                                        />
-                                    </li>}
-                                </Draggable>
+                                    <TaskPreview key={task.id}
+                                        id={task.id}
+                                        task={task}
+                                        groupId={group.id}
+                                        removeTask={removeTask}
+                                        saveTask={saveTask}
+                                        board={board}
+                                    />
+                                </li>}
+                        </Draggable>
                     )}
-                            {provided.placeholder}
+                    {provided.placeholder}
 
-                        </ul>
+                </ul>
             }
-                </Droppable >
-
-
+        </Droppable >
     )
 }
