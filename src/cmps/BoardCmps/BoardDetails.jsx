8683e019e331178ca318c04b3518dc8fb504cce1
yifatre <yifat.reg@gmail.com>
2024-04-09 10:54:08 +0300

    Fix add board not loading bug

--- src/cmps/BoardCmps/BoardDetails.jsx
+++ src/cmps/BoardCmps/BoardDetails.jsx
@@ -2,28 +2,20 @@ import { Outlet, useParams } from "react-router"
 import { GroupList } from "../GroupCmps/GroupList"
 import { useEffect, useState } from "react"
 
-import { showErrorMsg } from "../../services/event-bus.service"
 import { BoardHeader } from "./BoardHeader"
 import { BoardSideBar } from "./BoardSideBar"
-import { boardService } from "../../services/board/board.service.local"
 import { loadBoard, loadBoards, updateBoard } from "../../store/board/board.actions"
 import { useSelector } from "react-redux"
-import { ATTACHMENT, COVER, DATES, LABELS, MEMBERS } from "../TaskCmps/DynamicCmps/DynamicCmp"
 import { utilService } from "../../services/util.service"
 
 export function BoardDetails() {
     const { boardId } = useParams()
-    // const [board, setBoard] = useState(null)
 
     const board = useSelector(storeState => storeState.boardModule.board)
 
     useEffect(() => {
         getBoard()
-    }, [])
-
-    // useEffect(() => {
-    //     setBoard(boards[boards.findIndex(board => board._id === boardId)])
-    // }, [board])
+    }, [boardId])
 
     async function getBoard() {
         try {
@@ -31,7 +23,6 @@ export function BoardDetails() {
         }
         catch (err) {
             console.error(err)
-            // showErrorMsg('Cannot remove board')
         }
     }
 
@@ -70,38 +61,6 @@ export function BoardDetails() {
         const board = board.groups.filter(group => group.id !== groupId)
         updateBoard(board)
     }
-
-    // function onUpdateTask(cmp, info, task) {
-    //     //todo info:{groupId,taskId,dynamic}
-    //     var groupId = info.groupId
-    //     var taskToUpdate
-    //     switch (cmp) {
-    //         case LABELS:
-    //             taskToUpdate = { ...task, labelIds: info.dynamic }
-    //             // console.log('taskToUpdate:', taskToUpdate)
-    //             break
-
-    //         case MEMBERS:
-    //             taskToUpdate = { ...task, memberIds: info.dynamic }
-    //             // console.log('taskToUpdate', taskToUpdate)
-    //             break
-
-    //         case DATES:
-    //             taskToUpdate = { ...task, date: info.date }
-    //             break
-
-    //         case ATTACHMENT:
-    //             taskToUpdate = { ...task, attach: info.attach }
-    //             break
-
-    //         case COVER:
-    //             taskToUpdate = { ...task, cover: info.cover }
-    //             break
-    //     }
-    //     const groupToUpdate = board.groups.find(group => group.id === groupId)
-    //     return saveTask(taskToUpdate, groupToUpdate)
-    // }
-
 
     if (!board) return <div>loading</div>
     return (<>
