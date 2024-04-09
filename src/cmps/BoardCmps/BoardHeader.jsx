761c988174cb4f74c5b078b2f58b44cca7224491
yifatre <yifat.reg@gmail.com>
2024-04-09 11:15:04 +0300

    Add toggle star from board header

--- src/cmps/BoardCmps/BoardHeader.jsx
+++ src/cmps/BoardCmps/BoardHeader.jsx
@@ -1,12 +1,24 @@
-import { userDemoData } from "../../services/demo-data";
-import { AvatarList } from "../UtilCmps/AvatarList";
-import { arrow_down, board_icon, ellipsis_icon, filter_icon, flash_icon, group_icon, rocket_icon, share_icon, star, star_outline, table_icon } from "../UtilCmps/SVGs";
+import { userDemoData } from "../../services/demo-data"
+import { updateBoard } from "../../store/board/board.actions"
+import { AvatarList } from "../UtilCmps/AvatarList"
+import { arrow_down, board_icon, ellipsis_icon, filter_icon, flash_icon, group_icon, rocket_icon, share_icon, star, star_outline, table_icon } from "../UtilCmps/SVGs"
 
 export function BoardHeader({ board }) {
+    async function onToggleStar(ev) {
+        ev.stopPropagation()
+        ev.preventDefault()
+        try {
+            updateBoard({ ...board, isStarred: !board.isStarred }, true)
+        }
+        catch (err) {
+            console.error(err)
+        }
+    }
+
     return (
         <header className="board-header">
             <h1 className="board-title">{board.title}</h1>
-            <button className="board-star">{board.isStarred ? <span className="svg-container">{star}</span> : <span className="svg-container">{star_outline}</span> }</button>
+            <button className="board-star" onClick={onToggleStar}>{board.isStarred ? <span className="svg-container">{star}</span> : <span className="svg-container">{star_outline}</span>}</button>
             <button className="btn2 visibility">{group_icon}Workspace visible</button>
             <button className="btn2 board-view active">{board_icon}Board</button>
             <button className="btn2 table-view">{table_icon}Table</button>
@@ -16,7 +28,7 @@ export function BoardHeader({ board }) {
             <button className="btn2">{flash_icon}Automation</button>
             <button className="btn2">{filter_icon}Filters</button>
             <span className="sep"></span>
-            <div className="users-avatars"><AvatarList users={userDemoData} maxUsers={5}/></div>
+            <div className="users-avatars"><AvatarList users={userDemoData} maxUsers={5} /></div>
             <button className="btn2 board-share active">{share_icon}Share</button>
             <button className="board-options">{ellipsis_icon}</button>
         </header>
