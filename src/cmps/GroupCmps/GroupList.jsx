6f1ed3c9af030253281322150c767759a679ea2e
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-04-11 14:04:15 +0300

    i branch

--- src/cmps/GroupCmps/GroupList.jsx
+++ src/cmps/GroupCmps/GroupList.jsx
@@ -3,15 +3,121 @@ import { DragDropContext, Droppable, Draggable } from '@atlaskit/pragmatic-drag-
 import { DropIndicator } from '@atlaskit/pragmatic-drag-and-drop-react-drop-indicator/box-without-terminal';
 import { updateBoard } from "../../store/board/board.actions"
 import { utilService } from "../../services/util.service"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { plus_icon } from "../UtilCmps/SVGs"
 import { DynEntityAdd } from "../TaskCmps/DynEntityAdd"
+
+// import {
+//     dropTargetForExternal,
+//     monitorForExternal,
+// } from '@atlaskit/pragmatic-drag-and-drop/external/adapter';
+
+// import {
+//     dropTargetForElements,
+//     monitorForElements,
+// } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+
+// import {
+//     dropTargetForTextSelection,
+//     monitorForTextSelection,
+// } from '@atlaskit/pragmatic-drag-and-drop/text-selection/adapter';
+
+import { draggable, dropTargetForElements, monitorForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import { dropTargetForExternal } from '@atlaskit/pragmatic-drag-and-drop/external/adapter';
 
 export function GroupList({ board, saveGroup, removeGroup, saveTask, removeTask }) {
     const [isLabelsExtended, setIsLabelExtended] = useState(false)
     const [isAddGroup, setIsAddGroup] = useState(false)
+    const [draggableItem, setDraggableItem] = useState(false)
+    const [id, setId] = useState()
+    const ref = useRef(null);
     const { groups } = board
+    const [_groups, _setGroups] = useState(groups)
+    //   const [isDraggedOver, setIsDraggedOver] = useState(false);
 
+    // useEffect(()=> {
+
+    // },[_groups])
+
+
+    useEffect(() => {
+        return monitorForElements({
+            onDragStart({ source, location }) {
+                // source.element.style = {...source.element.style, background: 'blue'}
+                source.element = <div>sdkgljfgasfkd;a</div>
+                console.log(source);
+                console.log(source.element);
+                // setDraggableItem(source.data.idx)
+            },
+
+            onDrag({ source, location }) {
+                // console.log(source, location);
+                // const destination = location.current.dropTargets[0];
+                // if (!destination) {
+                //     // if dropped outside of any drop targets
+                //     return;
+                // }
+                // const destinationLocation = destination.data.idx;
+                // const sourceLocation = source.data.idx;
+                // const pieceType = source.data.type;
+
+                // const [group] = _groups.splice(sourceLocation, 1)
+                // // console.log('group', group);
+                // _groups.splice(destinationLocation, 0, group)
+                // // console.log('groups', _groups);
+                // _setGroups([..._groups])
+            },
+
+            onDrop({ source, location }) {
+                console.log('ondrop', source, location);
+                const destination = location.current.dropTargets[0];
+                if (!destination) {
+                    // if dropped outside of any drop targets
+                    return;
+                }
+                const destinationLocation = destination.data.idx;
+                const sourceLocation = source.data.idx;
+                const pieceType = source.data.type;
+
+                console.log('destinationLocation', destinationLocation);
+                console.log('sourceLocation', sourceLocation);
+                console.log('pieceType', pieceType);
+
+
+                const [group] = _groups.splice(sourceLocation, 1)
+                console.log('group', group);
+                _groups.splice(destinationLocation, 0, group)
+                // console.log('groups', _groups);
+                _setGroups([..._groups])
+                console.log('groups', groups);
+                updateBoard({ ...board, _groups })
+                // if (
+                //     // type guarding
+                //     !isCoord(destinationLocation) ||
+                //     !isCoord(sourceLocation) ||
+                //     !isPieceType(pieceType)
+                // ) {
+                //     return;
+                // }
+
+                // const piece = pieces.find(p =>
+                //     isEqualCoord(p.location, sourceLocation),
+                // );
+                // const restOfPieces = pieces.filter(p => p !== piece);
+
+                // if (
+                //     canMove(sourceLocation, destinationLocation, pieceType, pieces) &&
+                //     piece !== undefined
+                // ) {
+                //     // moving the piece!
+                //     setPieces([
+                //         { type: piece.type, location: destinationLocation },
+                //         ...restOfPieces,
+                //     ]);
+                // }
+            },
+        });
+    }, []);
 
     function onDragStart(abc) {
         // console.log('abc', abc)
@@ -41,57 +147,41 @@ export function GroupList({ board, saveGroup, removeGroup, saveTask, removeTask 
             const groupEnd = groups.find(group => group.id === result.destination.droppableId)
             const [task] = groupStart.tasks.splice(startIdx, 1)
             groupEnd.tasks.splice(endIdx, 0, task)
-            updateBoard({ ...board, groups })
+            // updateBoard({ ...board, groups })
         }
     }
 
     return (
-        <DragDropContext
-            // onBeforeCapture={onBeforeCapture}
-            // onBeforeDragStart={onBeforeDragStart}
-            onDragStart={onDragStart}
-            onDragUpdate={onDragUpdate}
-            onDragEnd={onDragEnd}
-        >
-            <Droppable droppableId={'groups'} direction='horizontal' type="group">
-                {(provided, snapshot, a, b) => (
-                    <ul className="clean-list flex group-list" {...provided.droppableProps} ref={provided.innerRef}>
-                        {groups?.map((group, idx) =>
-                            <Draggable key={group.id} draggableId={group.id} index={idx}>
-                                {(provided, snapshot) =>
-                                
-                                    <li key={group.id}
-                                        className="group-preview-container"
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        style={snapshot.isDragging ? { ...provided.draggableProps?.style, opacity: 0.6, rotate: '4deg', } : { ...provided.draggableProps?.style, cursor: 'pointer' }}
-                                    >
-                                        <GroupPreview
-                                            provided={provided}
-                                            group={group}
-                                            isLabelsExtended={isLabelsExtended}
-                                            setIsLabelExtended={setIsLabelExtended}
-                                            saveGroup={saveGroup}
-                                            saveTask={saveTask}
-                                            removeTask={removeTask}
-                                            board={board} />
-                                            {console.log('ab', a, b)}
-                                            <DropIndicator/>
-                                    </li>}
-                            </Draggable>
-                        )}
-                        {provided.placeholder}
-                        <li className="group-preview-container group-preview add-group-container">
-                            <div className="group-header flex">
-                                {!isAddGroup && <button className="add-group-btn group-preview" onClick={() => setIsAddGroup(true)}>{plus_icon}Add another list</button>}
-                                {isAddGroup && <div className="add-group group-preview"><DynEntityAdd setIsAddMode={setIsAddGroup} saveEntity={saveGroup} type={'GROUP'} /></div>}
 
-                            </div>
-                        </li>
-                    </ul>)}
-            </Droppable>
-        </DragDropContext>
+        <ul className="clean-list flex group-list">
+            {_groups?.map((group, idx) =>
+
+                // <li key={group.id}
+                //     className="group-preview-container"
+                //     ref={ref}
+                // >
+                <GroupPreview
+                    key={idx}
+                    idx={idx}
+                    group={group}
+                    isLabelsExtended={isLabelsExtended}
+                    setIsLabelExtended={setIsLabelExtended}
+                    saveGroup={saveGroup}
+                    saveTask={saveTask}
+                    removeTask={removeTask}
+                    board={board} />
+                // <DropIndicator />
+                // </li>
+            )}
+            <li className="group-preview-container group-preview add-group-container" >
+                <div className="group-header flex">
+                    {!isAddGroup && <button className="add-group-btn group-preview" onClick={() => setIsAddGroup(true)}>{plus_icon}Add another list</button>}
+                    {isAddGroup && <div className="add-group group-preview"><DynEntityAdd setIsAddMode={setIsAddGroup} saveEntity={saveGroup} type={'GROUP'} /></div>}
+
+                </div>
+            </li>
+        </ul>
+
     )
 }
 
--- src/cmps/GroupCmps/GroupPreview.jsx
+++ src/cmps/GroupCmps/GroupPreview.jsx
@@ -1,14 +1,15 @@
 import { ClickAwayListener } from '@mui/base/ClickAwayListener'
 import { TaskList } from '../TaskCmps/TaskList'
 import { collapse_icon, ellipsis_icon, plus_icon, create_icon, extend_icon } from '../UtilCmps/SVGs'
-import { useState } from 'react'
 import { TextareaAutosize as MinTextArea } from '@mui/base/TextareaAutosize'
 
 import { DynamicCmp, GROUP_ACTIONS } from '../TaskCmps/DynamicCmps/DynamicCmp'
 import { utilService } from '../../services/util.service'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from "react"
+import { draggable, dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
 
-export function GroupPreview({ group, saveGroup, board, isLabelsExtended, setIsLabelExtended, saveTask, removeTask }) {
+
+export function GroupPreview({ group, saveGroup, board, isLabelsExtended, setIsLabelExtended, saveTask, removeTask, idx }) {
     const [isAddMode, setIsAddMode] = useState(false)
     const [isEditTitle, setIsEditTitle] = useState(false)
     const [isExtended, setIsExtended] = useState(true)
@@ -16,6 +17,34 @@ export function GroupPreview({ group, saveGroup, board, isLabelsExtended, setIsL
     const [isActionsOpen, setIsActionsOpen] = useState(false)
     const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 })
     const scrollRef = useRef(null)
+    const refDrop = useRef(null);
+    const refDrag = useRef(null);
+    const [dragging, setDragging] = useState(false)
+    const [isDraggedOver, setIsDraggedOver] = useState(false);
+
+    useEffect(() => {
+        const el = refDrag.current;
+        // invariant(el);
+        return draggable({
+            element: el,
+            onDragStart: () => setDragging(true),
+            onDrop: () => setDragging(false),
+            getInitialData: () => ({idx, type:'group'})
+        })
+        
+    }, [idx])
+
+      useEffect(() => {
+        const el = refDrop.current;
+        // invariant(el);
+        return dropTargetForElements({
+          element: el,
+          onDragEnter: () => setIsDraggedOver(true),
+          onDragLeave: () => setIsDraggedOver(false),
+          onDrop: () => setIsDraggedOver(false),
+          getData: () => ({ idx }),
+        })
+      }, [])
 
     function handleClickAway() {
         setIsEditTitle(false)
@@ -34,40 +63,45 @@ export function GroupPreview({ group, saveGroup, board, isLabelsExtended, setIsL
         setIsActionsOpen(true)
     }
 
-    return (<>
-        {!isExtended && <li className={`group-preview-shrunken ${group.style.themeColor || 'neutral'}`} onClick={() => setIsExtended(true)}>
-            <button className="collapse">{extend_icon}</button>
-            <h2 className='group-title'>{group.title}</h2>
-            <h4 className='tasks-number'>{group.tasks.length}</h4>
-        </li>
-        }
-        {isExtended && <div className={`group-preview ${group.style.themeColor || 'neutral'} ${scrollRef.current?.scrollHeight > scrollRef.current?.clientHeight ? 'scrolling' : ''}`}>
+    return (
+        <li key={group.id}
+            className="group-preview-container"
+            ref={refDrop}
+            style={{ backgroundColor: isDraggedOver ? 'red' : '' }}
+        >
+            {!isExtended && <li className={`group-preview-shrunken ${group.style.themeColor || 'neutral'}`} onClick={() => setIsExtended(true)}>
+                <button className="collapse">{extend_icon}</button>
+                <h2 className='group-title'>{group.title}</h2>
+                <h4 className='tasks-number'>{group.tasks.length}</h4>
+            </li>
+            }
+            {isExtended && <div ref={refDrag} key={group.id} className={`group-preview ${group.style.themeColor || 'neutral'} ${scrollRef.current?.scrollHeight > scrollRef.current?.clientHeight ? 'scrolling' : ''}`}>
 
-            <div className="group-header">
-                {!isEditTitle && <h2 className="group-title" onClick={() => setIsEditTitle(true)}>{group.title}</h2>}
-                {isEditTitle &&
-                    <ClickAwayListener onClickAway={handleClickAway}>
-                        <MinTextArea className="title-edit" value={titleToEdit} autoFocus={true} onFocus={(ev) => ev.target.select()} onChange={handleHeaderChange} onKeyDown={(ev) => { if (ev.code === 'Enter') handleClickAway() }} ></MinTextArea>
-                    </ClickAwayListener>}
-                <button className="collapse" onClick={() => setIsExtended(false)}>{collapse_icon}</button>
-                <button className="options" onClick={onOpenActionsMenu}>{ellipsis_icon}</button>
-            </div>
-
-            <div className={`tasks-container ${scrollRef.current?.scrollHeight > scrollRef.current?.clientHeight ? 'scrolling' : ''}`} ref={scrollRef}>
-                <TaskList group={group} saveTask={saveTask} board={board} isLabelsExtended={isLabelsExtended} setIsLabelExtended={setIsLabelExtended} isAddMode={isAddMode} setIsAddMode={setIsAddMode} />
-            </div>
-
-            {!isAddMode && <div className='add'>
-                <button className="add-task" onClick={() => setIsAddMode(true)}>{plus_icon}Add a card</button>
-                <button className="create-from-template">{create_icon}</button>
-            </div>}
-        </div>}
-        {isActionsOpen &&
-            <ClickAwayListener onClickAway={() => setIsActionsOpen(false)}>
-                <div>
-                    <DynamicCmp setIsActionsOpen={setIsActionsOpen} cmp={GROUP_ACTIONS} position={modalPosition} group={group} saveGroup={saveGroup} />
+                <div className="group-header">
+                    {!isEditTitle && <h2 className="group-title" onClick={() => setIsEditTitle(true)}>{group.title}</h2>}
+                    {isEditTitle &&
+                        <ClickAwayListener onClickAway={handleClickAway}>
+                            <MinTextArea className="title-edit" value={titleToEdit} autoFocus={true} onFocus={(ev) => ev.target.select()} onChange={handleHeaderChange} onKeyDown={(ev) => { if (ev.code === 'Enter') handleClickAway() }} ></MinTextArea>
+                        </ClickAwayListener>}
+                    <button className="collapse" onClick={() => setIsExtended(false)}>{collapse_icon}</button>
+                    <button className="options" onClick={onOpenActionsMenu}>{ellipsis_icon}</button>
                 </div>
-            </ClickAwayListener >}
-    </>
+
+                <div className={`tasks-container ${scrollRef.current?.scrollHeight > scrollRef.current?.clientHeight ? 'scrolling' : ''}`} ref={scrollRef}>
+                    {/* <TaskList group={group} saveTask={saveTask} board={board} isLabelsExtended={isLabelsExtended} setIsLabelExtended={setIsLabelExtended} isAddMode={isAddMode} setIsAddMode={setIsAddMode} /> */}
+                </div>
+
+                {!isAddMode && <div className='add'>
+                    <button className="add-task" onClick={() => setIsAddMode(true)}>{plus_icon}Add a card</button>
+                    <button className="create-from-template">{create_icon}</button>
+                </div>}
+            </div>}
+            {isActionsOpen &&
+                <ClickAwayListener onClickAway={() => setIsActionsOpen(false)}>
+                    <div>
+                        <DynamicCmp setIsActionsOpen={setIsActionsOpen} cmp={GROUP_ACTIONS} position={modalPosition} group={group} saveGroup={saveGroup} />
+                    </div>
+                </ClickAwayListener >}
+        </li>
     )
 }
